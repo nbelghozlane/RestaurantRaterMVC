e043cf2f085a54c8ac8e737f4855d87e8d9b7c0f
Nadia <nbelghoz@gmail.com>
2021-03-24 16:03:13 -0400

    Added Details code/Finished walkthrough

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -93,5 +93,20 @@ namespace RestaurantRaterMVC.Controllers
             }
             return View(restaurant);
         }
+
+        //GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
